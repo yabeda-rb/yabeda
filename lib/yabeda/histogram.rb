aa2fe9b4dd401d06a91255506cfd17203e276485
Alexander Susikov <a.susikov@rocketguys.com>
2020-01-13 13:46:32 +0300

    Added building of tags for histogram

--- lib/yabeda/histogram.rb
+++ lib/yabeda/histogram.rb
@@ -7,9 +7,10 @@ module Yabeda
     option :buckets
 
     def measure(tags, value)
-      values[tags] = value
+      all_tags = ::Yabeda::Tags.build(tags)
+      values[all_tags] = value
       ::Yabeda.adapters.each do |_, adapter|
-        adapter.perform_histogram_measure!(self, tags, value)
+        adapter.perform_histogram_measure!(self, all_tags, value)
       end
       value
     end
