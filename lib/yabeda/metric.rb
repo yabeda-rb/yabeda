58def01d51c35ea7daa03032e7b581e31669a960
Andrey Novikov <envek@envek.name>
2020-08-06 19:44:39 +0300

    Fix Yabeda::Metric#get to account for default tags

--- lib/yabeda/metric.rb
+++ lib/yabeda/metric.rb
@@ -17,7 +17,7 @@ module Yabeda
 
     # Returns the value for the given label set
     def get(labels = {})
-      values[labels]
+      values[::Yabeda::Tags.build(labels)]
     end
 
     def values
--- spec/yabeda/metric_spec.rb
+++ spec/yabeda/metric_spec.rb
@@ -1,10 +1,42 @@
 # frozen_string_literal: true
 
 RSpec.describe Yabeda::Metric do
-  describe "#tags" do
-    subject { described_class.new(name, options).tags }
+  let(:metric)  { described_class.new(name, options) }
+  let(:name)    { "some_metric" }
+  let(:options) { { tags: %i[foo bar] } }
 
-    let(:name)    { "some_metric" }
+  describe "#get" do
+    subject { metric.get(tags) }
+
+    let(:tags) { { foo: "1", bar: "2" } }
+
+    before do
+      metric.instance_variable_set(:@values, { tags => 42 })
+    end
+
+    it { is_expected.to eq(42) }
+
+    it "returns nil for absent data" do
+      expect(metric.get({ whatever: "else" })).to be_nil
+    end
+
+    context "when default tags are set" do
+      before do
+        Yabeda.configure do
+          default_tag :bar, "2"
+        end
+        Yabeda.configure!
+      end
+
+      it "returns value with account for default tags values" do
+        expect(metric.get({ foo: "1" })).to eq(42)
+      end
+    end
+  end
+
+  describe "#tags" do
+    subject { metric.tags }
+
     let(:options) { {} }
 
     it { is_expected.to eq [] }
