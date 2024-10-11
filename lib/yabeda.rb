yabeda-rb/yabeda

commit 7df0d8eba880cd5d191f583d1112046cc856ba53

Allow to increment counters and measure histograms without tags
Using empty tags (`{}`) as default.

Resolves https://github.com/yabeda-rb/yabeda/issues/26

lib/yabeda/counter.rb
@@ -3,7 +3,7 @@
 module Yabeda
   # Growing-only counter
   class Counter < Metric
-    def increment(tags, by: 1)
+    def increment(tags = {}, by: 1)
       all_tags = ::Yabeda::Tags.build(tags, group)
       values[all_tags] += by
       adapters.each_value do |adapter|

lib/yabeda/gauge.rb
@@ -12,11 +12,11 @@ module Yabeda
       value
     end
 
-    def increment(tags, by: 1)
+    def increment(tags = {}, by: 1)
       set(tags, get(tags).to_i + by)
     end
 
-    def decrement(tags, by: 1)
+    def decrement(tags = {}, by: 1)
       set(tags, get(tags).to_i - by)
     end
   end

lib/yabeda/histogram.rb
@@ -7,7 +7,7 @@ module Yabeda
     option :buckets
 
     # rubocop: disable Metrics/MethodLength
-    def measure(tags, value = nil)
+    def measure(tags = {}, value = nil)
       if value.nil? ^ block_given?
         raise ArgumentError, "You must provide either numeric value or block for Yabeda::Histogram#measure!"
       end

lib/yabeda/summary.rb
@@ -5,7 +5,7 @@ module Yabeda
   # calculate averages, percentiles, and so on.
   class Summary < Metric
     # rubocop: disable Metrics/MethodLength
-    def observe(tags, value = nil)
+    def observe(tags = {}, value = nil)
       if value.nil? ^ block_given?
         raise ArgumentError, "You must provide either numeric value or block for Yabeda::Summary#observe!"
       end

spec/yabeda/counter_spec.rb
@@ -15,10 +15,15 @@ RSpec.describe Yabeda::Counter do
     end
     Yabeda.configure! unless Yabeda.already_configured?
     allow(Yabeda::Tags).to receive(:build).with(tags, anything).and_return(built_tags)
+    allow(Yabeda::Tags).to receive(:build).with({}, anything).and_return({})
     Yabeda.register_adapter(:test_adapter, adapter)
   end
 
   it { expect(increment_counter).to eq(metric_value) }
+
+  it "increments counter with empty tags if tags are not provided" do
+    expect { counter.increment }.to change { counter.values[{}] }.by(1)
+  end
 
   it "execute perform_counter_increment! method of adapter" do
     increment_counter

spec/yabeda/histogram_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Yabeda::Histogram do
     end
     Yabeda.configure! unless Yabeda.already_configured?
     allow(Yabeda::Tags).to receive(:build).with(tags, anything).and_return(built_tags)
+    allow(Yabeda::Tags).to receive(:build).with({}, anything).and_return({})
     Yabeda.register_adapter(:test_adapter, adapter)
   end
 
@@ -38,6 +39,11 @@ RSpec.describe Yabeda::Histogram do
     it "execute perform_histogram_measure! method of adapter" do
       measure_histogram
       expect(adapter).to have_received(:perform_histogram_measure!).with(histogram, built_tags, be_between(0.01, 0.05))
+    end
+
+    it "measures with empty tags if tags are not provided", :aggregate_failures do
+      histogram.measure(&block)
+      expect(adapter).to have_received(:perform_histogram_measure!).with(histogram, {}, be_between(0.01, 0.05))
     end
   end
 

spec/yabeda/summary_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Yabeda::Summary do
     Yabeda.configure { summary :test_summary }
     Yabeda.configure! unless Yabeda.already_configured?
     allow(Yabeda::Tags).to receive(:build).with(tags, anything).and_return(built_tags)
+    allow(Yabeda::Tags).to receive(:build).with({}, anything).and_return({})
     Yabeda.register_adapter(:test_adapter, adapter)
   end
 
@@ -36,6 +37,11 @@ RSpec.describe Yabeda::Summary do
     it "execute perform_summary_observe! method of adapter" do
       observe_summary
       expect(adapter).to have_received(:perform_summary_observe!).with(summary, built_tags, be_between(0.01, 0.05))
+    end
+
+    it "observes with empty tags if tags are not provided", :aggregate_failures do
+      summary.observe(&block)
+      expect(adapter).to have_received(:perform_summary_observe!).with(summary, {}, be_between(0.01, 0.05))
     end
   end
 

commit 5b890ed7281994206f072fa4c0031c880d2bc426

Fix counter increment for Ruby 2.x
Adding default values to arguments in 7df0d8eba880cd5d191f583d1112046cc856ba53 broke these methods in Ruby 2.x due to the differences in keyword arguments handling in Ruby 2.x that were fixed in Ruby 3.x

See https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/ for more details

Fixes https://github.com/yabeda-rb/yabeda/issues/40

lib/yabeda.rb
@@ -13,6 +13,8 @@ require "yabeda/errors"
 module Yabeda
   include DSL
 
+  EMPTY_TAGS = {}.freeze
+
   class << self
     extend Forwardable
 

lib/yabeda/counter.rb
@@ -3,7 +3,12 @@
 module Yabeda
   # Growing-only counter
   class Counter < Metric
-    def increment(tags = {}, by: 1)
+    # @overload increment(tags = {}, by: 1)
+    #   Increment the counter for given set of tags by the given increment value
+    #   @param tags Hash{Symbol=>#to_s} tags to identify the counter
+    #   @param by [Integer] strictly positive increment value
+    def increment(*args)
+      tags, by = self.class.parse_args(*args)
       all_tags = ::Yabeda::Tags.build(tags, group)
       values[all_tags] += by
       adapters.each_value do |adapter|
@@ -15,5 +20,25 @@ module Yabeda
     def values
       @values ||= Concurrent::Hash.new(0)
     end
+
+    # @api private
+    # rubocop:disable Metrics/MethodLength
+    def self.parse_args(*args)
+      case args.size
+      when 0 # increment()
+        [EMPTY_TAGS, 1]
+      when 1 # increment(by: 5) or increment(tags)
+        if args[0].key?(:by)
+          [EMPTY_TAGS, args.fetch(:by)]
+        else
+          [args[0], 1]
+        end
+      when 2 # increment(tags, by: 5)
+        [args[0], args[1].fetch(:by, 1)]
+      else
+        raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 0..2)"
+      end
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end

lib/yabeda/gauge.rb
@@ -12,11 +12,21 @@ module Yabeda
       value
     end
 
-    def increment(tags = {}, by: 1)
+    # @overload increment(tags = {}, by: 1)
+    #   Convenience method to increment current gauge value for given set of tags by the given increment value
+    #   @param tags Hash{Symbol=>#to_s} tags to identify the gauge
+    #   @param by [Integer] increment value
+    def increment(*args)
+      tags, by = Counter.parse_args(*args)
       set(tags, get(tags).to_i + by)
     end
 
-    def decrement(tags = {}, by: 1)
+    # @overload decrement(tags = {}, by: 1)
+    #   Convenience method to decrement current gauge value for given set of tags by the given decrement value
+    #   @param tags Hash{Symbol=>#to_s} tags to identify the gauge
+    #   @param by [Integer] decrement value
+    def decrement(*args)
+      tags, by = Counter.parse_args(*args)
       set(tags, get(tags).to_i - by)
     end
   end