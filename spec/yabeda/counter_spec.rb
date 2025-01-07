88c3c3fa124f323a1a65be606f8fa5dcad76b729
Alexander Marychev <killondark@gmail.com>
2025-01-07 12:53:21 +0300

    Fixed randomly fail tests

--- spec/yabeda/counter_spec.rb
+++ spec/yabeda/counter_spec.rb
@@ -6,31 +6,25 @@ RSpec.describe Yabeda::Counter do
   let(:tags) { { foo: "bar" } }
   let(:metric_value) { 10 }
   let(:adapter) { instance_double(Yabeda::BaseAdapter, perform_counter_increment!: true, register!: true) }
+  let(:counter) { Yabeda.test_counter }
 
   before do
     Yabeda.register_adapter(:test_adapter, adapter)
+    Yabeda.configure do
+      counter :test_counter
+    end
+    Yabeda.configure! unless Yabeda.already_configured?
   end
 
-  context "when config has no group" do
-    let(:counter) { Yabeda.test_counter }
+  it { expect(increment_counter).to eq(metric_value) }
 
-    before do
-      Yabeda.configure do
-        counter :test_counter
-      end
-      Yabeda.configure! unless Yabeda.already_configured?
-    end
+  it "increments counter with empty tags if tags are not provided" do
+    expect { counter.increment }.to change { counter.values[{}] }.by(1)
+  end
 
-    it { expect(increment_counter).to eq(metric_value) }
-
-    it "increments counter with empty tags if tags are not provided" do
-      expect { counter.increment }.to change { counter.values[{}] }.by(1)
-    end
-
-    it "execute perform_counter_increment!" do
-      increment_counter
-      expect(adapter).to have_received(:perform_counter_increment!).with(counter, tags, metric_value)
-    end
+  it "execute perform_counter_increment!" do
+    increment_counter
+    expect(adapter).to have_received(:perform_counter_increment!).with(counter, tags, metric_value)
   end
 
   context "with adapter option" do
